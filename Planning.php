cec073919f9e8a770edfdddf08ad198c4fd74805
Sanderbraat <60172884+Sanderbraat@users.noreply.github.com>
2020-02-07 11:57:47 +0100

    Add files via upload

---
+++ Planning.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <title>Vliegtuig </title>
+      <link rel="Stylesheet" href="menu.css">
+    </head>
+    <body>
+    <div class="welcometxt"> On The Fly </div>
+    <div class="onderwelcometxt"> Planning </div>
+        <div class="Button1"><a class="linkstijl" href="http://localhost//vliegvlieg/vlieg.php"><button type="submit" class="Button" name=btn>Home</button></a><div/>
+          <div class="Button2"><a class="linkstijl" href="http://localhost//vliegvlieg/Vliegtuigen.php"><button type="submit" class="Button" name=btn>Vliegtuig</button></a><div/>
+        <div class="Button3"><a class="linkstijl" href="http://localhost//vliegvlieg/Planning.php"><button type="submit" class="Button" name=btn>Planning</button></a><div/>
+
+    </body>
+</html>
---
+++ Vliegtuigen.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Vliegtuig </title>
+      <link rel="Stylesheet" href="menu.css">
+    </head>
+    <body>
+    <div class="welcometxt"> On The Fly </div>
+    <div class="onderwelcometxt"> Vliegtuigen </div>
+        <div class="Button1"><a class="linkstijl" href="http://localhost//vliegvlieg/vlieg.php"><button type="submit" class="Button" name=btn>Home</button></a><div/>
+        <div class="Button2"><a class="linkstijl" href="http://localhost//vliegvlieg/Vliegtuigen.php"><button type="submit" class="Button" name=btn>Vliegtuig</button></a><div/>
+        <div class="Button3"><a class="linkstijl" href="http://localhost//vliegvlieg/Planning.php"><button type="submit" class="Button" name=btn>Planning</button></a><div/>
+
+            <div class="txtboxes">
+            Vliegtuignummer: <input type="text" name="airplanenumber"><br/>
+            Type: <input type="text" name="type"><br/>
+            Vliegmaatschappij: <input type="text" name="airline"><br/>
+            Status: <input type="text" name="Statuss"><br/>
+            </div>
+    </body>
+</html>
+
+
